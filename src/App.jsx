e1113f0887af658699532b13e9663e3f0a91d93c
brunosandri <brunomsandri@gmail.com>
2025-07-23 11:45:32 -0400

    rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// src/App.jsx
+import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/Cadastro';
 
-function App() {
+export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/registrar' element={<RegisterPage />} />
-        {/* …outras rotas */}
-      </Routes>
-    </BrowserRouter>
+    /* ⛔️  NÃO coloque BrowserRouter aqui */
+    <Routes>
+      <Route path='/' element={<LandingPage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/registrar' element={<RegisterPage />} />
+      {/* …outras rotas */}
+    </Routes>
   );
 }
 
-export default App; 
